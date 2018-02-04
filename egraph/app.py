1b5c4d99bc4a2a9eef61edd05af1f0a678af6018
Masayuki Igawa <masayuki@igawa.io>
2018-02-04 16:46:48 +0900

    [WIP] snapshot commit

--- egraph/app.py
+++ egraph/app.py
@@ -15,31 +15,33 @@ def get_app():
     return app
 
 def format_for_highcharts(df):
-    """
-    see https://www.highcharts.com/demo
-    :param df: pd.DataFrame
-    :return:
-    """
+    #df.index = list(df.index.fillna('NODATA'))
+    #categories = list(df.columns.fillna('NODATA'))
     print(df)
-    df.index = list(df.index.fillna('NODATA'))
-    categories = list(df.columns.fillna('NODATA'))
 
-    df = df.fillna(0)
-    series = [{'name': i, 'data': list(map(str, row))} for i, row in df.iterrows()]
-    return {'categories': categories, 'series': series}
+    #df = df.fillna(0)
+    #for i, d in df.iteritems():
+        #print(i, d)
+        #print("==== %s" % (i.replace('kW', '')))
+    #series = [{'name': i, 'data': list(map(str, row.replace('kw', '')))} for i, row in df.iterrows()]
+    data = []
+    for d in df.iteritems():
+        data.append(d)
+    series = [{'name': i, 'data': row} for i, row in df.iteritems()]
+    return {'key': 'usage', 'values': [0.1,0.2,0.3,0.4] }
 
 
 @app.route("/", methods=['GET'])
 def index():
-    print(os.getcwd())
     return flask.render_template('index.html')
 
 
 @app.route("/graph", methods=['GET'])
 def graph():
-    print(os.getcwd())
-    df = pd.read_csv('sample_data/tepco.csv', index_col=0)
-    return flask.jsonify(format_for_highcharts(df.T))
+    df = pd.read_csv('sample_data/tepco.csv', index_col=4)
+    #print(df)
+    #df = df.reset_index(drop=True)
+    return flask.jsonify(format_for_highcharts(df.iloc[0:,8]))
 
 def parse_command_line_args():
     description = 'Starts the API service for egraph'
@@ -63,7 +65,7 @@ def main():
     try:
         port = config.getint('default', 'port')
     except (configparser.NoOptionError, KeyError, configparser.NoSectionError):
-        port = 5000
+        port = 8000
     app.run(debug=True, host=host, port=port)
 
 
